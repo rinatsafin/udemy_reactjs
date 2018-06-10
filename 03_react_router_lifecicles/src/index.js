451ed21848125ac4b581de9d334ccd9bcdf0d2a0
rinatsafin <safinrinat87@gmail.com>
2018-06-10 18:43:11 +0300

    lesson 26 MemoryRouter

--- 03_react_router_lifecicles/src/index.js
+++ 03_react_router_lifecicles/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Link } from "react-router-dom";
+import { MemoryRouter, Route, Link } from "react-router-dom";
 
 // Components
 import Home from "./components/Home";
@@ -10,7 +10,7 @@ import PostItem from "./components/Post_item";
 
 const App = () => {
   return (
-    <HashRouter>
+    <MemoryRouter>
       <div>
         <header>
           <Link to="/">Home</Link>
@@ -26,7 +26,7 @@ const App = () => {
         <Route path="/posts/:id/:username" component={PostItem} />
         <Route path="/profile" component={Profile} />
       </div>
-    </HashRouter>
+    </MemoryRouter>
   );
 };
 
